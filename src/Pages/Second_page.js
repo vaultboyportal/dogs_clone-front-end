830024c3342eca1de6311b284c846fd2fa97cdf8
kraievskyi <bogdan.1233211233213@gmail.com>
2024-07-27 21:45:18 +0300

    First def page commit

---
+++ src/Pages/Second_page.js
@@ -0,0 +1,14 @@
+// src/components/SecondPage.js
+
+import React from "react";
+
+const SecondPage = () => {
+    return (
+        <div className="second-page">
+            <h1>Друга сторінка</h1>
+            <p>Це ваша друга сторінка.</p>
+        </div>
+    );
+};
+
+export default SecondPage;
