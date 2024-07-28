8ccf32a0c0a8626e1e10ce77df95175b56b75107
kraievskyi <bogdan.1233211233213@gmail.com>
2024-07-28 20:57:23 +0300

    fix csses

--- src/Pages/bottomNavbar.js
+++ src/Pages/bottomNavbar.js
@@ -47,7 +47,7 @@ const BottomNavbar = () => {
                 >
                     <path
                         d="M192 64C86 64 0 150 0 256S86 448 192 448h256c106 0 192-86 192-192S554 64 448 64H192zM496 168a40 40 0 1 1 0 80 40 40 0 1 1 0-80zM392 304a40 40 0 1 1 80 0 40 40 0 1 1 -80 0zM168 200c0-13.3 10.7-24 24-24s24 10.7 24 24v32h32c13.3 0 24 10.7 24 24s-10.7 24-24 24h-32v32c0 13.3-10.7 24-24 24s-24-10.7-24-24v-32h-32c-13.3 0-24-10.7-24-24s10.7-24 24-24h32v-32z"
-                        fill={isActive ? "#ffffff" : "#A6A6A6"}
+                        fill={isActive("/game") ? "#ffffff" : "#A6A6A6"}
                         strokeWidth="2"
                         strokeLinecap="round"
                         strokeLinejoin="round"
@@ -79,7 +79,7 @@ const BottomNavbar = () => {
             </div>
 
             <div
-                className={`_navbarItem_1cr97_20 ${isActive("/friends") ? "_isActive_1cr97_41" : ""}`}
+                className={`_navbarItem_1cr97_20 ${isActive("/invite") ? "_isActive_1cr97_41" : ""}`}
                 onClick={() => navigate("/invite")}
             >
                 <svg
